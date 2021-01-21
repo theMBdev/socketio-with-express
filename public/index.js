5a027df86c165cafc568bc5464c425247bae8e46
Martin Brown <35014201+martingit9@users.noreply.github.com>
2021-01-21 00:00:48 +0000

    Messaging and user list kina

--- public/index.js
+++ public/index.js
@@ -29,8 +29,43 @@ async function main() {
     const welcomeElement = document.getElementById("welcome");
     socket.on("online", online => onlineElement.innerText = online.toString());
 
+    // same as above but onld syntax
     const onlineElement = document.getElementById("online");
     socket.on("welcome", welcomeMessage => welcomeElement.innerText = welcomeMessage);
+    
+    // socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
+
+    const outputElement = document.getElementById("output");
+    socket.on("onlineCLients", onlineClentsOutput => outputElement.innerText = JSON.stringify(onlineClentsOutput));
+
+
+
+
+
+    // Messaging
+    var messages = document.getElementById('messages');
+      var form = document.getElementById('form');
+      var input = document.getElementById('input');
+
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (input.value) {
+          socket.emit('chat message', input.value);
+          input.value = '';
+        }
+      });
+
+      socket.on('chat message', function(msg) {
+        var item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+
+
+
 }
 
 main();
+
+
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+const { Console } = require("console");
 const
     http = require("http"),
     express = require("express"),
@@ -15,7 +16,10 @@ function generateRandomNumber() {
 
 function onNewWebsocketConnection(socket) {
     console.info(`Socket ${socket.id} has connected.`);
+    console.log("oc", onlineClients);
     onlineClients.add(socket.id);
+
+    // console.log("OnlineClients", onlineClients);
 
     socket.on("disconnect", () => {
         onlineClients.delete(socket.id);
@@ -27,9 +31,20 @@ function onNewWebsocketConnection(socket) {
 
     // will send a message only to this socket (different than using `io.emit()`, which would broadcast it)
     socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
+
+   
+
+   
+
+
+
+
+
 }
 
 function startServer() {
+    console.log("starting server")
+
     // create a new express app
     const app = express();
     // create http server and wrap the express app
@@ -47,7 +62,9 @@ function startServer() {
     io.on("connection", onNewWebsocketConnection);
 
     // important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
+    // server.listen(process.env.PORT, () => console.info(`Listening on port ${process.env.PORT}.`));
     server.listen(SERVER_PORT, () => console.info(`Listening on port ${SERVER_PORT}.`));
+    console.log("server started")
 
     // will send one message per second to all its clients
     let secondsSinceServerStarted = 0;
@@ -55,7 +72,26 @@ function startServer() {
         secondsSinceServerStarted++;
         io.emit("seconds", secondsSinceServerStarted);
         io.emit("online", onlineClients.size);
+
+         // turn set object into val readable
+    var item;
+    var onlineClientsAsObject = ''
+    for (item of onlineClients.values())
+    onlineClientsAsObject+=item + ' '; 
+
+        io.emit("onlineCLients", onlineClientsAsObject)
     }, 1000);
+
+
+    io.on('connection', (socket) => {
+        socket.on('chat message', msg => {
+          io.emit('chat message', msg);
+        });
+      });
+
+
+
 }
 
 startServer();
+
